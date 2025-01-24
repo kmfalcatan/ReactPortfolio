cf0b23aa143c14d1b4e3c26bc12a29cc865f7439
khrizmarr falcatan <kmfalcatan2Gmail.com>
2025-01-24 13:06:08 +0800

    akjsdksj

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import Hero from './components/hero'
-import Body from './components/body'
-import './App.css'
-function App() {
+import Hero from "./components/hero";
+import Loading from "./components/loadingScreen";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 
+function App() {
   return (
-    <>
-     <Hero/> 
-     <Body/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoadingPage />} />
+        <Route path="/hero" element={<Hero />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+function LoadingPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/hero");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return <Loading />;
+}
+
+export default App;
