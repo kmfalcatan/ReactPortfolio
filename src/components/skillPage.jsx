66c7a038f82cfd717e80aa5f8443f5a3d48f9db2
khrizmarr falcatan <kmfalcatan2Gmail.com>
2025-01-31 12:11:40 +0800

    skills

--- src/components/skillPage.jsx
+++ src/components/skillPage.jsx
@@ -7,6 +7,111 @@ export default function Skill() {
         <p className="text2">Background Skills and Accomplishment</p>
         <p className="text3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat corporis, vel ut neque hic recusandae dolore nulla, impedit facere laborum, eius sit veritatis porro provident repudiandae rerum non tenetur ab.</p>
       </div>
+
+      <div className="logoContainer">
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img className="logo" src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>HTML</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>CSS</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>JAVASCRIPT</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>REACT.JS</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>TAILWIND</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>FIGMA</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>CANVA</p>
+          </div>
+        </div>
+
+        
+        <div className="logoTitleContainer">
+          <div className="subLogoContainer">
+            <img src="" alt="" />
+          </div>
+
+          <div className="textContainer4">
+            <p>VS CODE</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="percentContainer">
+        <div className="devContainer">
+
+        </div>
+
+        <div className="toolContainer">
+          <div className="subToolContainer">
+
+          </div>
+
+          <div className="subToolContainer">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
