1bf01effaf8c42d78fa38d298299da91378f0185
khrizmarr falcatan <kmfalcatan2Gmail.com>
2025-02-18 20:54:46 +0800

    modify about

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,9 +4,6 @@ import Header from "./header";
 import Background from "./background";
 import Home from "./hero";
 import About from "./aboutPage";
-import Skill from "./skillPage";
-import Project from "./project";
-import Contact from "./contact";
 
 export default function body() {
   return (
@@ -20,18 +17,6 @@ export default function body() {
 
       <div id="about" className="aboutContainer">
         <About/>
-      </div>
-
-      <div id="skill" className="skillContainer">
-        <Skill/>
-      </div>
-
-      <div id="project" className="projectContainer">
-        <Project/>
-      </div>
-
-      <div id="contact" className="contactContainer">
-        <Contact/>
       </div>
     </div>
   );
