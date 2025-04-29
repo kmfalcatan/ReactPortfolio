6b57f765228d7d060661c0ddd43719e94fc93f9c
khrizmarr falcatan <kmfalcatan2Gmail.com>
2025-04-30 01:04:23 +0800

    add light mode and dark mode not yet done

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../style/loading.css";
 import "../style/hero.css";
 import Header from "./header";
@@ -8,38 +9,34 @@ import Skill from "./skillPage";
 import Project from "./project";
 import Contact from "./contact";
 import Blog from "./blog";
-import Experience from "./experience"
+import Experience from "./experience";
 
 export default function Body() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="container">
-      <Background />
-      <Header />
+    <div className={`container ${darkMode ? "dark" : "light"}`}>
+      <Background darkMode={darkMode} />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
 
       <div id="home" className="homeContainer">
-        <Home />
+        <Home darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
-
       <div id="about" className="aboutContainer">
         <About />
       </div>
-
       <div className="skillContainer" id="skill">
         <Skill />
       </div>
-
       <div id="project" className="projectContainer">
         <Project />
       </div>
-
       <div id="blog" className="blogContainer">
         <Blog />
       </div>
-
       <div id="experience" className="experienceContainer">
         <Experience />
       </div>
-
       <div id="contact" className="contactContainer">
         <Contact />
       </div>
